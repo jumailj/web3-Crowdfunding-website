76506b886920715b850f19ef9dbd89a4274252cc
jumailj <jumailj.work@gmail.com>
2023-03-24 20:20:12 +0530

    setup web3 settings

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router} from 'react-reouter-dom';
+import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
+
